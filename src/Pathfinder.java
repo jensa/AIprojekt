b2f41211e3e4ba547c4f6cf0bb535f10f567a7aa
Jens Arvidsson <jensarv@gmail.com>
2012-10-05 12:39:58 +0200

    started working on better pathfinding

---
+++ src/Pathfinder.java
@@ -0,0 +1,181 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+
+public class Pathfinder {
+	
+	/*
+	 * Disclaimer:
+	 * The pushable/pullabe paths for now at least assume that if a box has two opposite
+	 * walkable sides, it is pushable in both directions.
+	 * In reality, it would need a path from the players position to the pushing position in order to
+	 * do so.
+	 * 
+	 * I don't think this is a problem, becuase the path chosen will always be the shortest possible,
+	 * so weird (and unpushable) paths will be excluded by default. I think.
+	 */
+	
+	public enum WalkMode { WALK, PUSH, PULL }
+	
+	public static String findPullablePath (Coords from, Coords to, Board board){
+		return findPath (from, to, board, WalkMode.PULL);
+	}
+	
+	public static String findPushablePath (Coords from, Coords to, Board board){
+		return findPath (from, to, board, WalkMode.PUSH);
+	}
+	
+	public static String findWalkablePath (Coords from, Coords to, Board board){
+		return findPath (from, to, board, WalkMode.WALK);
+	}
+	
+	/**
+	 * Finds a walkable path from A to B, and returns it as a series of direction chars (U,D,L,R)
+	 * @param from
+	 * @param to
+	 * @param board
+	 * @return
+	 */
+	public static String findPath (Coords from, Coords to, Board board, WalkMode mode){
+		Stack<Coords> queue = new Stack<Coords> ();
+		int[][] counterMap = new int[board.getWidth()][board.getHeight()];
+		queue.push(from);
+		counterMap[from.x][from.y] = 1;
+		while (!queue.isEmpty()){
+			Coords c = queue.pop();
+			if (c.equals(to))
+				break;
+			ArrayList<Coords> cells = AdjacentCells (c, board, mode);
+			for (int i=0; i<cells.size();i++){
+				Coords adjacentCell = cells.get(i);
+				boolean lowerCounterExists = counterMap [adjacentCell.x][adjacentCell.y] > 0 &&
+						counterMap [adjacentCell.x][adjacentCell.y] < counterMap [c.x][c.y]+1;
+				if (lowerCounterExists){
+					cells.remove(i);
+					i--;
+				}
+				else{
+					counterMap [adjacentCell.x][adjacentCell.y] = counterMap [c.x][c.y]+1;
+				}
+			}
+			for (Coords newCoords : cells)
+				queue.push(newCoords);
+		}
+		Tools.printCounter (counterMap);
+		//p("to, x: " + to.x + " y: " + to.y);
+		return extractPath (from, to, counterMap);
+	}
+
+	private static String extractPath(Coords from, Coords to, int[][] counterMap) {
+		if (to.x == from.x && to.y == from.y) {
+			return "";
+		}
+		int nextX = to.x;
+		int nextY = 0;
+		String dir = "";
+		int nextBestScore = 1000;
+		if (counterMap[to.x+1][to.y] > 0 && counterMap[to.x+1][to.y] < nextBestScore|| (from.x == to.x+1 && from.y == to.y)) {
+			nextX = to.x+1;
+			nextY = to.y;
+			nextBestScore = counterMap[nextX][nextY];
+			dir = "L";
+		}	
+		if (counterMap[to.x-1][to.y] > 0 && counterMap[to.x-1][to.y] < nextBestScore|| (from.x == to.x-1 && from.y == to.y)) {
+			nextX = to.x-1;
+			nextY = to.y;
+			nextBestScore = counterMap[nextX][nextY];
+			dir = "R";
+		}
+		if (counterMap[to.x][to.y+1] > 0 && counterMap[to.x][to.y+1] < nextBestScore || (from.x == to.x && from.y == to.y +1)) {
+			nextX = to.x;
+			nextY = to.y+1;
+			nextBestScore = counterMap[nextX][nextY];
+			dir = "U";
+		}
+		if (counterMap[to.x][to.y-1] > 0 && counterMap[to.x][to.y-1] < nextBestScore || (from.x == to.x && from.y == to.y -1)) {
+			nextX = to.x;
+			nextY = to.y-1;
+			nextBestScore = counterMap[nextX][nextY];
+			dir = "D";
+		}
+		if (dir=="")
+			return null;
+		else
+			return extractPath(from, new Coords(nextX, nextY), counterMap)+dir;
+	}
+	
+	/**
+	 * Returnera en lista på rutor vi kan stå på.
+	 * @param cell
+	 * @return
+	 */
+	private static ArrayList<Coords> AdjacentCells(Coords cell, Board board, WalkMode mode) {
+		switch (mode){
+
+		case PULL: return pullableAdjacentCells (cell, board);
+		case PUSH: return pushableAdjacentCells (cell, board);
+		default: return walkableAdjacentCells (cell, board);
+		}
+	}
+	
+	private static ArrayList<Coords> pullableAdjacentCells(Coords cell,
+			Board b) {
+		ArrayList<Coords> cells = new ArrayList<Coords> ();
+		Coords up = new Coords (cell.x-1, cell.y);
+		Coords down = new Coords (cell.x+1, cell.y);
+		Coords left = new Coords (cell.x, cell.y-1);
+		Coords right = new Coords (cell.x, cell.y+1);
+		if (b.isTileWalkable(up) && b.isTileWalkable(down)){
+			addCell (up, cells, b);
+			addCell (down, cells, b);
+		}
+		if (b.isTileWalkable(left) && b.isTileWalkable(right)){
+			addCell (left, cells, b);
+			addCell (right, cells, b);
+		}
+		return cells;
+	}
+
+	private static ArrayList<Coords> pushableAdjacentCells(Coords cell,
+			Board b) {
+		ArrayList<Coords> cells = new ArrayList<Coords> ();
+		Coords up = new Coords (cell.x-1, cell.y);
+		Coords down = new Coords (cell.x+1, cell.y);
+		Coords left = new Coords (cell.x, cell.y-1);
+		Coords right = new Coords (cell.x, cell.y+1);
+		if (b.isTileWalkable(up) && b.isTileWalkable(down)){
+			addCell (up, cells, b);
+			addCell (down, cells, b);
+		}
+		if (b.isTileWalkable(left) && b.isTileWalkable(right)){
+			addCell (left, cells, b);
+			addCell (right, cells, b);
+		}
+		return cells;
+	}
+
+	public static void addCell (Coords c, ArrayList<Coords> cells, Board b){
+		try{
+			cells.add(c);
+		} catch(ArrayIndexOutOfBoundsException e){
+
+		}
+	}
+
+	/**
+	 * Returnera en lista på rutor vi kan stå på.
+	 * @param cell
+	 * @return
+	 */
+	private static ArrayList<Coords> walkableAdjacentCells(Coords cell, Board board) {
+		ArrayList<Coords> list = Tools.createAdjacentCells(cell, board);
+		for (int i = 0; i < list.size(); i++) {
+			if (!board.isTileWalkable(list.get(i))) {
+				list.remove(i);
+				i--;
+			}
+		}
+		return list;
+	}
+
+}
